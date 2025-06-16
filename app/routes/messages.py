f0b86b86e5f5e860d3739546320f95c1ba5fd2bd
tenshialk <mariacicera0981@gmail.com>
2025-06-16 16:38:39 +0000

    13:38

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -3,7 +3,6 @@ from app.models.message import Message
 
 messages_bp = Blueprint('messages', __name__)
 
-
 @messages_bp.route("/", methods=["GET"])
 def get_messages():
     messages = Message.query.order_by(Message.created_at.desc()).all()
